62e3788309d5b7d42917c32405c6aecfadc6caaf
unknown <Bhavik.b@brilworks.com>
2025-02-24 13:45:50 +0530

    HAHA

--- lib/flutter_flow/flutter_flow_util.dart
+++ lib/flutter_flow/flutter_flow_util.dart
@@ -384,6 +384,8 @@ void fixStatusBarOniOS16AndBelow(BuildContext context) {
 
 extension ColorOpacityExt on Color {
   Color applyAlpha(double val) => withValues(alpha: val);
+  
+  withValues({required double alpha}) {}
 }
 
 String roundTo(double value, int decimalPoints) {
